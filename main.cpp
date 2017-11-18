5e1b4c52efc31f378163d186f0a9549ab949be05
hndada <33769671+hndada@users.noreply.github.com>
2017-11-18 16:56:28 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,22 @@
-Block* wBlocks = new Block[13];
-Block* bBlocks = new Block[13];
+Block* Blocks = new Block[26];
 for (int i=0; i < 12; i++)
-	wBlocks[i] = Block(i, W);
+	Blocks[i] = Block(i, W);
 for (int i=0; i < 12; i++)
-	bBlocks[i] = Block(i, B);
+	Blocks[i+13] = Block(i, B);
 
 
 Block::Block(int n_out, char color_)
 {
 	n_outside=n_out;
-	if n_out!=-1
+	if (n_out!=-1)
 	{
-		if color_=B
+		if (color_=B)
 			n_array=n_outside;
-		if color_=W
+		if (color_=W)
 			n_array=n_outside+0.5;
 	}
 	color=color_;
-
+}
 
 
 
